a7cb882641d0f5ec43ebc0beb625cc62ed170e08
kishanshettyb <kishanqr@gmail.com>
2024-03-04 18:30:22 +0530

    add page

---
+++ src/pages/pricingpolicy.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Layout from "../components/Layout";
+
+const pricingpolicy = () => {
+	return (
+		<Layout>
+			<Container>
+				<div className="mt-5">
+					<h2 className="mb-4">Pricing Policy:</h2>
+					<p>
+						At "I And You Being Together," we strive to offer fair and transparent pricing for all our services. Our pricing policy is designed to reflect the value of the quality
+						instruction and guidance provided in our Yoga and Dhyan Meditation courses.
+					</p>
+					<h4>Course Fees:</h4>
+					<p>
+						Our course fees are determined based on factors such as the duration of the program, the expertise of our instructors, and the resources required to deliver a
+						fulfilling experience. We believe in providing excellent value for your investment in your well-being.
+					</p>
+					<p>Workshop Registrations:</p>
+					<p>
+						For workshops and special events, registration fees may vary depending on the nature of the event, guest instructors, and additional amenities provided. We ensure that
+						the registration fees are competitive and commensurate with the benefits and experiences offered.
+					</p>
+					<p>Merchandise Sales:</p>
+					<p>
+						We may offer merchandise such as yoga mats, meditation cushions, and apparel for sale. Our prices for merchandise are set to cover production costs and ensure
+						reasonable profit margins while maintaining affordability for our clients.
+					</p>
+					<p>Discounts and Promotions:</p>
+					<p>
+						Periodically, we may offer discounts or promotions on our courses, workshops, or merchandise. These promotions may be advertised through our website, social media
+						channels, or email newsletters. Terms and conditions for discounts and promotions will be clearly communicated at the time of the offer.
+					</p>
+					<p>Payment Methods:</p>
+					We accept various payment methods, including credit/debit cards, online payments, and cash (if applicable). Payment details and options will be provided during the
+					booking or registration process.
+					<p>Changes to Pricing:</p>
+					<p>
+						While we strive to maintain consistent pricing, we reserve the right to adjust prices for our services and merchandise as necessary. Any changes to pricing will be
+						communicated in advance to our clients through our website and other relevant channels.
+					</p>
+					<p>
+						Thank you for choosing "I And You Being Together" for your wellness journey. If you have any questions or concerns regarding our pricing policy, please feel free to
+						contact us.
+					</p>
+					<p>Sincerely,</p>
+					<p>[Your Organization Name]</p>
+				</div>
+			</Container>
+		</Layout>
+	);
+};
+
+export default pricingpolicy;
