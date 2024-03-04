6dd6c44bad8764d4a3025738f3a87aa0269c3fbb
kishanshettyb <kishanqr@gmail.com>
2024-03-04 16:42:46 +0530

    fix issue

--- src/pages/eshwaridhyana.js
+++ src/pages/eshwaridhyana.js
@@ -16,22 +16,26 @@ const eshwaridhyana = () => {
 					<div className="text-center  mb-5">
 						<StaticImage src="../images/others/line-2.png" alt="line" className="line" />
 					</div>
-					<Row className="d-flex align-items-center">
-						<Col md={6}>
-							<p>
-								Eshwari dhyana is come from a good old Shaktha tradition, Eshwari Dhyana is described as a simple, natural, effortless Dhyana technique that doesn't involve much
-								concentration. One uses a Powerful Energized Dhyana mantra as a vehicle to let the mind settle down naturally in its source .
-							</p>
-							<p>
-								In this regard, the main goal of Eshwari Dhyana is to have one's thoughts in the present moment, the process involves transcending thought itself and experiencing a
-								state of pure awareness in which one is aware but without an object of thought or feelings, a subjective experience during meditation are clearly reflected in the
-								contrasting neurophysiologic states associated with each practice. Eshwari Dhyana has been found to activate the brain's default mode network, which is a natural
-								resting state of the brain regular practice of Eshwari Dhyana can result in a state of cosmic consciousness, in which the experience of transcendence is always
-								present in one's awareness, even during activity, with greater clarity and focus, One comes to experience one's self as universal and omnipresent. One's identity
-								shifts from the individual to the cosmic. Eshwari Dhyana is a simple , effortless yet effective Shaktha method
-							</p>
+					<Row className="d-flex align-items-center justify-content-start">
+						<Col md={4}>
+							<StaticImage src="../images/banners/eshwari-dhyana.jpeg" alt="Eshwari Dhyana" className="br-20 img-fluid" />
 						</Col>
-						<Col md={6}></Col>
+						<Col md={6}>
+							<div className="ms-0 ms-md-5">
+								<p>
+									Eshwari dhyana is come from a good old Shaktha tradition, Eshwari Dhyana is described as a simple, natural, effortless Dhyana technique that doesn't involve much
+									concentration. One uses a Powerful Energized Dhyana mantra as a vehicle to let the mind settle down naturally in its source .
+								</p>
+								<p>
+									In this regard, the main goal of Eshwari Dhyana is to have one's thoughts in the present moment, the process involves transcending thought itself and experiencing
+									a state of pure awareness in which one is aware but without an object of thought or feelings, a subjective experience during meditation are clearly reflected in
+									the contrasting neurophysiologic states associated with each practice. Eshwari Dhyana has been found to activate the brain's default mode network, which is a
+									natural resting state of the brain regular practice of Eshwari Dhyana can result in a state of cosmic consciousness, in which the experience of transcendence is
+									always present in one's awareness, even during activity, with greater clarity and focus, One comes to experience one's self as universal and omnipresent. One's
+									identity shifts from the individual to the cosmic. Eshwari Dhyana is a simple , effortless yet effective Shaktha method
+								</p>
+							</div>
+						</Col>
 					</Row>
 				</Container>
 			</section>
